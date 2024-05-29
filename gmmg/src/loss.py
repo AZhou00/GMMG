b492ea799b71cce9924fbf93009bc16863c668d8
AZhou00 <junzhezhou000@gmail.com>
2024-05-29 01:02:05 -0400

    refactored tokenization code. example with tfds

---
+++ gmmg/src/loss.py
@@ -0,0 +1,64 @@
+"""Common losses used in training GANs and masked modeling."""
+
+from typing import Any, Optional
+import jax
+import jax.numpy as jnp
+
+
+def squared_euclidean_distance(
+    a: jnp.ndarray, b: jnp.ndarray, b2: jnp.ndarray = None, precision: Any = None
+) -> jnp.ndarray:
+    """Computes the pairwise squared Euclidean distance.
+
+    Args:
+      a: float32: (n, d): An array of points.
+      b: float32: (m, d): An array of points.
+      b2: float32: (d, m): b square transpose.
+      precision: use DEFAULT precision by default
+
+    Returns:
+      d: float32: (n, m): Where d[i, j] is the squared Euclidean distance between
+      a[i] and b[j].
+    """
+    if b2 is None:
+        b2 = jnp.sum(b.T**2, axis=0, keepdims=True)
+    a2 = jnp.sum(a**2, axis=1, keepdims=True)
+    ab = jnp.matmul(a, b.T, precision=precision)
+    d = a2 - 2 * ab + b2
+    return d
+
+
+def entropy_loss(affinity, loss_type="softmax", temperature=1.0):
+    """Calculates the entropy loss."""
+    flat_affinity = affinity.reshape(-1, affinity.shape[-1])
+    flat_affinity /= temperature
+    probs = jax.nn.softmax(flat_affinity, axis=-1)
+    log_probs = jax.nn.log_softmax(flat_affinity + 1e-5, axis=-1)
+    if loss_type == "softmax":
+        target_probs = probs
+    elif loss_type == "argmax":
+        codes = jnp.argmax(flat_affinity, axis=-1)
+        onehots = jax.nn.one_hot(
+            codes, flat_affinity.shape[-1], dtype=flat_affinity.dtype
+        )
+        onehots = probs - jax.lax.stop_gradient(probs - onehots)
+        target_probs = onehots
+    else:
+        raise ValueError("Entropy loss {} not supported".format(loss_type))
+    avg_probs = jnp.mean(target_probs, axis=0)
+    avg_entropy = -jnp.sum(avg_probs * jnp.log(avg_probs + 1e-5))
+    sample_entropy = -jnp.mean(jnp.sum(target_probs * log_probs, axis=-1))
+    loss = sample_entropy - avg_entropy
+    return loss
+
+
+def l1_loss(y_true, y_pred):
+    diff = y_true - y_pred
+    diff = jnp.asarray(diff, jnp.float32)
+    return jnp.mean(jnp.abs(diff))
+
+
+def l2_loss(y_true, y_pred):
+    diff = y_true - y_pred
+    diff = jnp.asarray(diff, jnp.float32)
+    return jnp.mean(jnp.square(diff))
